64440b316b08522670b3ec30567bb9008933eedb
smmanjun <smithamanjunath26@gmail.com>
2020-04-11 15:27:11 +0530

    added bye

--- src/main/java/com/thangaali/common/utils/Dummy.java
+++ src/main/java/com/thangaali/common/utils/Dummy.java
@@ -2,6 +2,10 @@ package com.thangaali.common.utils;
 
 public class Dummy {
     public static void sayHello() {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! ");
+
+    }
+    public static void sayBye(){
+        System.out.println("Bye Bye");
     }
 }
